d763f9197fe28dbd7781652ba07d00dc204c8a1e
Md. Sheybul Alif Sakib <119123958+codersakib1@users.noreply.github.com>
2024-05-19 21:39:12 +0600

    Add files via upload

---
+++ Digital input output.ino
@@ -0,0 +1,14 @@
+int led = 13;
+int inp = 3;
+int state;
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  pinMode(inp, INPUT);
+}
+void loop()
+{
+  state = digitalRead(inp);
+  digitalWrite(led, state);
+}
